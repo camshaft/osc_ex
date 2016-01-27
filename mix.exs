f45b3282bb08f5f90f5966dcc7c3b176d25c58df
camshaft <bytheway.cameron@gmail.com>
2016-01-27 01:20:46 -0700

    add package to mix.exs

--- mix.exs
+++ mix.exs
@@ -5,9 +5,11 @@ defmodule Osc.Mixfile do
     [app: :osc,
      version: "0.1.0",
      elixir: "~> 1.0",
+     description: "OSC encoder/decoder for elixir",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps,
+     package: package,]
   end
 
   def application do
@@ -17,5 +19,12 @@ defmodule Osc.Mixfile do
   defp deps do
     [{:excheck, "~> 0.3.2", only: [:dev, :test]},
      {:triq, github: "krestenkrab/triq", only: [:dev, :test]}]
+  end
+
+  defp package do
+    [files: ["lib", "mix.exs", "README*"],
+     maintainers: ["Cameron Bytheway"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/camshaft/osc_ex"}]
   end
 end
